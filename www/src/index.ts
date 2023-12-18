b82fbc0c02fdff3d525d5dbe16f00a586fba8bf5
lvk88 <15655519+lvk88@users.noreply.github.com>
2023-12-18 19:33:59 +0100

    Add toggle button to change the field that is being postprocessed

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,5 +1,12 @@
 import { Simulation } from 'mywasm';
 
+enum PostprocField{
+  Temperature,
+  Phi
+}
+
+let postprocField = PostprocField.Temperature;
+
 const width = 100;
 const height = 100;
 
@@ -9,12 +16,32 @@ const canvas = <HTMLCanvasElement>document.getElementById("postproc-area");
 canvas.width = canvasScale * width;
 canvas.height = canvasScale * height;
 
+const toggleFieldButton = <HTMLButtonElement>document.getElementById("toggle-field");
+
+toggleFieldButton.addEventListener("click", () => {
+  if(postprocField == PostprocField.Temperature){
+    postprocField = PostprocField.Phi;
+  } else {
+    postprocField = PostprocField.Temperature;
+  }
+  postprocess();
+});
+
 const context = canvas.getContext("2d");
 
 const s = new Simulation(width, height);
 
-const rgbBuffer = s.get_phi_rgb();
-const rgbDataArray = new Uint8ClampedArray(rgbBuffer);
-const imageData = new ImageData(rgbDataArray, width, height);
-const bitmap = await createImageBitmap(imageData);
-context.drawImage(bitmap, 0, 0, 3 * width, 3 * height);
+const postprocess = async () => {
+  let rgbBuffer: Uint8Array = null;
+  if(postprocField == PostprocField.Phi){
+    rgbBuffer = s.get_phi_rgb();
+  } else {
+    rgbBuffer = s.get_temperature_rgb();
+  }
+  const rgbDataArray = new Uint8ClampedArray(rgbBuffer);
+  const imageData = new ImageData(rgbDataArray, width, height);
+  const bitmap = await createImageBitmap(imageData);
+  context.drawImage(bitmap, 0, 0, 3 * width, 3 * height);
+}
+
+postprocess();
