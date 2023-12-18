be0a866b7f4a3d167764c3800a58661008bc76c0
lvk88 <15655519+lvk88@users.noreply.github.com>
2023-12-18 19:33:58 +0100

    Add array2d, grid and griddata classes

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,60 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C"{
-    fn alert(msg: &str);
+pub struct Array2D{
+    size: [usize; 2],
+    data: Vec<f64>
 }
 
-#[wasm_bindgen]
-pub fn get_name() -> String{
-    "Laszlo".into()
+pub struct Grid{
+    delta: [f64; 2],
+    size: [usize; 2],
 }
 
-#[wasm_bindgen]
-pub fn alert_user(){
-    alert(&get_name()[..]);
+pub struct GridData<'a>{
+    grid: &'a Grid,
+    data: Array2D
+}
+
+impl<'a> GridData<'a>{
+    pub fn new(grid: &Grid, data: Array2D) -> GridData{
+        GridData{
+            grid,
+            data
+        }
+    }
+}
+
+impl Grid{
+    pub fn new(delta: [f64; 2], size: [usize; 2]) -> Grid{
+        Grid{
+            delta,
+            size
+        }
+    }
+}
+
+impl Array2D{
+    pub fn new(size: [usize; 2]) -> Array2D{
+        let data = vec![0.; size[0] * size[1]];
+        Array2D{
+            size,
+            data
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_float_eq::*;
+
+    #[test]
+    fn create_a_grid() {
+        let grid = Grid::new([0.1, 0.2], [3, 4]);
+        let data = Array2D::new(grid.size);
+
+        let grid_data = GridData::new(&grid, data);
+
+        assert_f64_near!(grid_data.data.data[0], 0.);
+    }
 }
